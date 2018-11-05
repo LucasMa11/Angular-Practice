d83c44e9e29a587001441fb4f68aabb8c7bd88d6
Lucas Ma <mayh11@lenovo.com>
2018-11-05 18:32:46 +0800

    1

--- js/main.js
+++ js/main.js
@@ -19,18 +19,50 @@ function Spread(sectionname) {
     var Sp = document.getElementById(recom);
     var ht = document.querySelector("body");
     if (Sp && fs && ht) {
-        var rect = fs.getBoundingClientRect();
-        Sp.style.top = rect.top + 'px';
-        Sp.style.display = 'block';
-        ht.style.overflow = 'hidden';
+        var animation = fs.animate({
+            transform: ['scale(1)', 'scale(0.99)'],
+        }, {
+            direction: 'alternate',
+            duration: 500,
+            iterations: 1
+        });
+        animation.play();
+        setTimeout(function () {
+            var rect = fs.getBoundingClientRect();
+            Sp.style.top = rect.top + 'px';
+            Sp.style.display = 'block';
+            ht.style.overflow = 'hidden';
+        }, 500);
     }
 }
 function narrow(backname) {
-    var bna = 'rc_' + backname.charAt(backname.length - 1);
-    var na = document.getElementById(bna);
+    var fs = '.section_' + backname.charAt(backname.length - 1);
+    var na = document.getElementById(backname);
     var ht = document.querySelector("body");
-    if (na && ht) {
+    var fss = document.querySelector(fs);
+    fss.style.visibility = 'visible';
+    na.scrollTop = 0;
+    na.classList.add('disrecommed');
+    na.addEventListener("animationend", function () {
+        na.classList.remove('disrecommed');
         na.style.display = 'none';
         ht.style.overflow = 'auto';
-    }
+    }, { once: true });
+}
+function inter(introcename) {
+    var na = document.getElementById(introcename);
+    na.style.display = 'block';
+}
+function back(back_name) {
+    var na = document.getElementById(back_name);
+    var nb = na;
+    nb.classList.add('disapper');
+    // setTimeout(()=>{
+    //     nb.style.display='none';
+    //     nb.classList.remove('disapper');
+    // },1000)
+    nb.addEventListener("animationend", function () {
+        nb.classList.remove('disapper');
+        nb.style.display = 'none';
+    }, { once: true });
 }
--- ts/main.ts
+++ ts/main.ts
@@ -17,25 +17,65 @@ function Spread(sectionname:string)
 {
     let sec='.'+sectionname;
     let recom='rc_'+sec.charAt(sec.length-1);
-    let fs:HTMLElement | null = document.querySelector(sec);
-    let Sp:HTMLElement | null = document.getElementById(recom);
-    let ht:HTMLElement | null = document.querySelector("body");
+    let fs:any = document.querySelector(sec);
+    let Sp:any = document.getElementById(recom);
+    let ht:any = document.querySelector("body");
     if(Sp&&fs&&ht){
+        let animation =fs.animate({
+            transform: ['scale(1)', 'scale(0.99)'],
+        },{
+            direction: 'alternate',
+            duration: 500,
+            iterations: 1
+        });
+        animation.play();
+    setTimeout(()=>{
         let rect: ClientRect = fs.getBoundingClientRect();
         Sp.style.top = rect.top+'px';
         Sp.style.display = 'block';
         ht.style.overflow = 'hidden';
+    },500)
+       
+        
     }
 }
 
 function narrow(backname:string)
 {
-    let bna='rc_'+backname.charAt(backname.length-1);
-    let na:HTMLElement | null = document.getElementById(bna);
-    let ht:HTMLElement | null = document.querySelector("body");
-    if(na&&ht){
-        na.style.display = 'none';
+    let fs ='.section_'+backname.charAt(backname.length-1) ;
+    let na:any = document.getElementById(backname);
+    let ht:any = document.querySelector("body");
+    let fss:any = document.querySelector(fs);
+    fss.style.visibility ='visible';
+    na.scrollTop=0;
+    na.classList.add('disrecommed');
+    na.addEventListener("animationend",function(){
+        na.classList.remove('disrecommed');
+        na.style.display='none';
         ht.style.overflow = 'auto';
-    }
-    
+    },{once:true});
 }
+
+function inter(introcename:string)
+{
+    let na:any=document.getElementById(introcename);
+    na.style.display = 'block';
+}
+
+function back(back_name:string)
+{
+    let na:any=document.getElementById(back_name);
+    let nb:HTMLElement = na;
+    nb.classList.add('disapper');
+    // setTimeout(()=>{
+    //     nb.style.display='none';
+    //     nb.classList.remove('disapper');
+    // },1000)
+    nb.addEventListener("animationend",function(){
+        nb.classList.remove('disapper');
+        nb.style.display='none';
+    },{once:true});
+}
+
+
+
